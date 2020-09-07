1fbcf1c55a6c865ca5d73d0d3552490723d6c3b5
Dibyadeep Bhattacharya <dibyadeepbhattacharyya1998@gmail.com>
2020-09-07 20:27:37 +0530

    Initial commit without the nominate button

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useReducer, useEffect } from "react";
+
+import Header from "./components/header.component";
+import Movie from "./components/movie.component";
+import spinner from "./assets/ajax-loader.gif";
+import Search from "./components/search.component";
+import { initialState, reducer } from "./store/reducer";
+import axios from "axios";
+import './App.css';
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    axios.get(MOVIE_API_URL).then(jsonResponse => {
+      dispatch({
+        type: "SEARCH_MOVIES_SUCCESS",
+        payload: jsonResponse.data.Search
+      });
+    });
+  }, []);
+
+  const search = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+
+    axios(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`).then(
+      jsonResponse => {
+        if (jsonResponse.data.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIES_SUCCESS",
+            payload: jsonResponse.data.Search
+          });
+        } else {
+          dispatch({
+            type: "SEARCH_MOVIES_FAILURE",
+            error: jsonResponse.data.Error
+          });
+        }
+      }
+    );
+  };
+
+  const { movies, errorMessage, loading } = state;
+
+  const retrievedMovies =
+    loading && !errorMessage ? (
+      <img className="spinner" src={spinner} alt="Loading spinner" />
+    ) : errorMessage ? (
+      <div className="errorMessage">{errorMessage}</div>
+    ) : (
+      movies.map((movie, index) => (
+        <Movie key={`${index}-${movie.Title}`} movie={movie} />
+      ))
+    );
+
+  return (
+    <div className="App">
+      <div className="m-container">
+        <Header text="MOVIE NOMINATION APP" />
+
+        <Search search={search} />
+
+        <p className="App-intro">Nominate a few of your favourite movies</p>
+
+        <div className="movies">{retrievedMovies}</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
